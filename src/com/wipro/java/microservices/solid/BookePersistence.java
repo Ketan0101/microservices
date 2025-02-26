3a1e3607bfa9dfc690a2a5a04e74f493c683aa36
Ketan0101 <100190989+Ketan0101@users.noreply.github.com>
2025-02-26 10:34:57 +0530

    First commit

---
+++ src/com/wipro/java/microservices/solid/BookePersistence.java
@@ -0,0 +1,5 @@
+package com.wipro.java.microservices.solid;
+
+public class BookePersistence {
+
+}
---
+++ src/com/wipro/java/microservices/solid/InvoicePersistence.java
@@ -0,0 +1,6 @@
+package com.wipro.java.microservices.solid;
+//This interface will increase modularity
+public class InvoicePersistence {
+//This method will save the information to the database
+	
+}
